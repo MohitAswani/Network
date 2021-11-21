f270632edae86cdd6f30882d5720502e60e010f8
Mohit Aswani <72434608+MohitAswani@users.noreply.github.com>
2021-11-21 08:24:46 +0530

    Adding log in , sign up and sign out

--- app/src/main/java/com/example/network/Login.kt
+++ app/src/main/java/com/example/network/Login.kt
@@ -1,11 +1,59 @@
 package com.example.network
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.network.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
+
+    private lateinit var mAuth:FirebaseAuth
+
+    private lateinit var binding:ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding= ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mAuth= FirebaseAuth.getInstance()
+
+        binding.signupButton.setOnClickListener{
+            val intent= Intent(this,Signup::class.java)
+            startActivity(intent)
+        }
+
+        binding.logInButton.setOnClickListener{
+            val email=binding.editEmail.text.toString()
+            val password=binding.editPassword.text.toString()
+
+            login(email,password)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(mAuth.currentUser!=null)
+        {
+            val intent=Intent(this@Login,MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun login(email:String,password:String)
+    {
+        // logging in the user
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // code after successful login
+                    val intent=Intent(this@Login,MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@Login,"USER DOES NOT EXIST",Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/example/network/MainActivity.kt
+++ app/src/main/java/com/example/network/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.network
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.network.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    private lateinit var mAuth:FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mAuth= FirebaseAuth.getInstance()
+
+        binding.logOutButton.setOnClickListener{
+            mAuth.signOut()
+            val intent= Intent(this@MainActivity,Login::class.java)
+            startActivity(intent)
+        }
     }
 }
